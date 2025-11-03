a0e93ebd31d77438fee5d503c8fa0d339c71c4b2
dhimashf <muhammaddhimas1410@gmail.com>
2025-11-03 22:45:30 +0700

    logging info

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import uvicorn
 from common.config.settings import settings
+import logging
+
+# --- Konfigurasi Logging Global ---
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 if __name__ == "__main__":
     # Server FastAPI sekarang mengelola siklus hidup bot melalui lifespan events.
     # Tidak perlu lagi menjalankan bot di thread terpisah.
     print(f"Starting FastAPI server at http://{settings.APP_HOST}:{settings.APP_PORT}")
     # Gunakan file backend/app.py sebagai target uvicorn. `reload` diaktifkan hanya untuk development.
-    uvicorn.run("backend.app:app", host=settings.APP_HOST, port=settings.APP_PORT, reload=settings.DEBUG)
+    uvicorn.run("backend.app:app", host=settings.APP_HOST, port=settings.APP_PORT, reload=False) # Set reload ke False untuk tes OAuth
